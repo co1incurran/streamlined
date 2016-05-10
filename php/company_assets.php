51fd2c94e9da3bb2bba5e023c44f72e6693900e1
Colin Curran <col.w.curran@gmail.com>
2016-05-10 15:42:50 +0100

    shows the assets when the asset button is clicked

--- php/company_assets.php
+++ php/company_assets.php
@@ -1,15 +1,38 @@
 <?php
-
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "user");
+define("DB_PASSWORD", "1234");
+define("DB_DATABASE", "database");
+ 
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 $companyid = $_GET['companyid'];
 $companyid = mysqli_real_escape_string($con ,$companyid);
+$sql = "SELECT * FROM company WHERE companyid ='$companyid' ; ";
+$res = mysqli_query($con,$sql);
+$result = array();
 
 
 $sql2 = "SELECT * FROM `stock` WHERE stockid IN (SELECT stockid FROM uses WHERE jobid IN 
 (SELECT jobid FROM jobs WHERE jobid IN (SELECT jobid FROM company_requires WHERE companyid = '$companyid'))); ";
 $res2 = mysqli_query($con,$sql2);
 $result2 = array();
-
+ 
+while($row = mysqli_fetch_array($res)){
+	array_push($result,
+		array('companyid'=>$row[0],
+		'name'=>$row[1],
+		'address_line1'=>$row[2],
+		'address_line2'=>$row[3],
+		'address_line3'=>$row[4],
+		'address_line4'=>$row[5],
+		'county'=>$row[6],
+		'country'=>$row[7],
+		'sage_id'=>$row[8]
+	));
+}
+ //print_r (array_values($result));
+ //echo '<br>';
 while($row = mysqli_fetch_array($res2)){
 	array_push($result2,
 		array('stockid'=>$row[0],
@@ -28,21 +51,68 @@ while($row = mysqli_fetch_array($res2)){
 		'last_results'=>$row[13]
 	));
 } //print_r (array_values($result2));
-echo '							 
-											 <ul id="business-card-menu">
-											 <li class= "current-tab"><a href>Assets</a></li>
-											 <li><a>Contacts</a></li>
-											 <li><a>History</a></li>
-											 <li><a>Notes</a></li>
-											 </ul>
+//echo '<br>';
+
+foreach ($result as $results){
+	echo '<div class="main-section">
+				
+					<div class="container-fluid no-padding">
+						<div class="col-md-7 no-padding">
+							<div class="main-content panel panel-default no-margin">
+								<header class="panel-heading clearfix">
+
+									 <span class="avatar"></span>
+									 <hgroup>';
+										/*<a href="documentation/index.html" class="btn btn-default pull-right" rel="#overlay"><i class="fa fa-question-circle"></i></a>';*/
+											 echo	'<h2>'. ucwords($results['name']).'<br></h2>';
+												$ad1 = ucwords($results['address_line1']);
+												$ad2 = ucwords($results['address_line2']);
+												$ad3 = ucwords($results['address_line3']);
+												$ad4 = ucwords($results['address_line4']);
+												$county = ucwords($results['county']);
+												$country = ucwords($results['country']);
+												if(!empty($ad1)){ 
+													echo $ad1.', ';
+													//echo nl2br("\n");
+												}
+												if(!empty($ad2) && $ad2 != $ad1){ 
+													echo $ad2.', ';
+													//echo nl2br("\n");
+												}
+												if(!empty($ad3)&& $ad3 != $ad2 && $ad3 != $ad1){ 
+													echo $ad3.', ';
+													//echo nl2br("\n");
+												}
+												if(!empty($ad4)&& $ad4 != $ad3 && $ad4 != $ad2 && $ad4 != $ad1){ 
+													echo $ad4.', ';
+													//echo nl2br("\n");
+												}
+												if(!empty($county)&& $county != $ad4 && $county != $ad3){ 
+													echo $county.', ';
+													//echo nl2br("\n");
+												}
+												if(!empty($country)){ 
+													echo $country;
+												}
+												//to count the number of assets 
+											 $counter = 0;
+											 foreach ($result2 as $results2){
+												 $counter ++;
+											 }
+											 //menu for the business card
+											 
+											 echo '<h3>Assets: '.$counter.'</h3>
+								</hgroup>
+								</header>
+								<section class="panel-body" style = "width:100%">';
+									echo'		 
 									<table align="center">
 										<th><tr class = "blue-row">
 										<td class = "asset-list"><strong>Product</strong></td>
 										<td class = "asset-list"><strong>Model</strong></td>
 										<td class = "asset-list"><strong>Manufacturer</strong></td>
 										<td class = "asset-list"><strong>Install Date</strong></td>
-										<td class = "asset-list"><strong>Inspection Date</strong></td>
-										<td class = "asset-list"><strong>Service Due</strong></td>
+										<td class = "asset-list"><strong>Last Service</strong></td>
 										<td class = "asset-list"><strong>Serial Number</strong></td>
 										</tr></th>';
 										$i = 1;
@@ -56,15 +126,9 @@ echo '
 										<td class = "asset-list">'. ucwords($results2['name']) . '</td>
 										<td class = "asset-list">'. ucwords($results2['model']) . '</td>
 										<td class = "asset-list">'. ucwords($results2['manufacturer']) . '</td>';
-										
-										$date = $results2['installation_date'];
-										$properDate = date("d/m/Y", strtotime($date));
-										echo '<td class = "asset-list">'. ($properDate) . '</td>';
-										
-										$date1 = $results2['inspection_date'];
+										$date1 = $results2['installation_date'];
 										$properDate1 = date("d/m/Y", strtotime($date1));
 										echo '<td class = "asset-list">'. ($properDate1) . '</td>';
-										
 										$date2 = $results2['service_date'];
 										$properDate2 = date("d/m/Y", strtotime($date2));
 										echo '<td class = "asset-list">'. ($properDate2) . '</td>
@@ -77,7 +141,7 @@ echo '
 							</div>
 						</div>';
 			echo '</div>
-		</div>';
-
-
+			</div>';
+}
+mysqli_close($con);
 ?>
