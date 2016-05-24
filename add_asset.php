5b96174588ab06c695d1a790e4860b89acf487af
Colin Curran <col.w.curran@gmail.com>
2016-05-24 16:56:17 +0100

    not finished

---
+++ add_asset.php
@@ -0,0 +1,115 @@
+<?php
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "user");//remember to chanege these when all is working
+define("DB_PASSWORD", "1234");
+define("DB_DATABASE", "database");
+ 
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+$url= $_GET['url'];
+$customerid= $_GET['customerid'];
+$companyid= $_GET['companyid'];
+
+//to get the job number
+if($companyid != 0){
+	$sql = "SELECT job_number FROM jobs WHERE jobid IN (SELECT jobid FROM company_requires WHERE companyid = $companyid);";
+}else{
+	$sql = "SELECT job_number FROM jobs WHERE jobid IN (SELECT jobid FROM customer_requires WHERE customerid = $customerid);";
+}
+$result = mysqli_query($con,$sql);
+mysqli_close($con);
+echo'
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Add asset</title>
+	<link href="css/elements.css" rel="stylesheet">
+	<script src="js/popup.js"></script>
+	</head>
+<!-- Body Starts Here -->
+	<body>
+	<div id="body" style="overflow:hidden;">
+		<div id="abc">
+			<!-- Popup Div Starts Here -->
+			<div id="popupContact">
+			<!-- Contact Us Form -->
+				<form action="save_asset.php" id="form" method="post" name="form">
+					<!--<img id="close" src="images/3.png" onclick ="div_hide()">-->
+					<h2>Add asset</h2>
+					<hr>
+					<input type="hidden" name="url" id="url" value="'.$url.'">
+					<input type="hidden" name="customerid" id="customerid" value="'.$customerid.'">
+					<input type="hidden" name="companyid" id="companyid" value="'.$companyid.'">
+					
+					<label for="jobnumber"><small>Job number</small></label>
+					<select id="jobnumber" class="drop_down" name = "jobnumber" class="form-control" required>';
+					if (mysqli_num_rows($result) > 0) {
+						while($row = mysqli_fetch_assoc($result)) {
+							echo'<option value="'.$row["job_number"].'">'.$row["job_number"].'</option>';
+						}
+						echo '<option value= "not available">Not available</option>';
+					}else{
+						echo '<option value= "not available">Not available</option>';
+					}
+					echo'
+					</select><br>
+					
+					<label for="serialnumber"><small>Serial number</small></label>
+					<input id="serialnumber" name="serialnumber" type="text" maxlength = "50"  required>
+					
+					<label for="type"><small>Product type</small></label>
+					<input id="type" name="type" type="text" maxlength = "50" required>
+					
+					<label for="model"><small>Model</small></label>
+					<input id="model" name="model" type="text" maxlength = "50"  required>
+					
+					<label for="manufacturer"><small>Manufacturer</small></label>
+					<input id="manudacturer" name="manufacturer" type="text" maxlenght = "50">
+					
+					<label for="productdescription"><small>Product description</small></label>
+					<input id="productdescription" name="productdescription" type="text">
+					
+					<label for="installdate"><small>Install date</small></label>
+					<input id="installdate" name="installdate" type="date" required>
+					
+					<label for="inspectiondue"><small>Inspection due</small></label>
+					<input id="inspectiondue" name="inspectiondue" type="date">
+					
+					<label for="servicedue"><small>Service due</small></label>
+					<input id="servicedue" name="servicedue" type="date" required>
+					
+					<label for="location"><small>Location <small> eg Room 20</small></small></label>
+					<input id="location" name="location" type="text" maxlength = "65" required>
+					
+					<label for="contracttype"><small>Service contract type</small></label>
+					<select id="contracttype" class="drop_down" name = "contracttype" class="form-control">
+						<option value= "not specified">Not specified</option>
+						<option value= "a">A</option>
+						<option value= "b">B</option>
+						<option value= "c">C</option>
+						<option value= "d">D</option>
+						<option value= "pay as you go">Pay as you go</option>
+						<option value= "not with us">Not with us</option>
+						<option value= "off contract">Off contract</option>
+					</select><br>
+					
+					<label for="renewaldate"><small>Contract renewal date</small></label>
+					<input id="renewaldate" name="renewaldate" type="date">
+					
+					<label for="fundedby"><small>Maintenance funded by</small></label>
+					<input id="fundedby" name="renewaldate" type="text" required>
+					
+					<input type="submit" id="submit" value="Save">
+					<!--<a href="javascript:%20check_empty()" id="submit">Save</a>-->
+					<a onclick="goBack()" id="submit">Cancel</a>
+				</form>
+			</div>
+		<!-- Popup Div Ends Here -->
+		</div>
+	</div>
+	</body>
+	<script type="text/javascript">
+	window.onload = div_show();
+	</script>
+<!-- Body Ends Here -->
+</html>';
+?>
---
+++ save_asset.php
@@ -0,0 +1,139 @@
+<?php
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "user");//remember to chanege these when all is working
+define("DB_PASSWORD", "1234");
+define("DB_DATABASE", "database");
+ 
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+//Back URL
+$url= $_POST["url"];
+
+//COMPANYID
+$companyid = $_POST["companyid"];
+$companyid = trim($companyid);
+$filtercompanyid = filter_var($companyid, FILTER_VALIDATE_INT);
+$cleancompanyid = mysqli_real_escape_string($con, $filtercompanyid);
+//echo $cleancompanyid.'<br>';
+
+//CUSTOMERID
+$customerid = $_POST["customerid"];
+$customerid = trim($customerid);
+$filtercustomerid = filter_var($customerid, FILTER_SANITIZE_STRING);
+$cleancustomerid= mysqli_real_escape_string($con, $filtercustomerid);
+//echo $cleancustomerid.'<br>';
+
+//First name
+$firstname = $_POST["firstname"];
+$firstname = trim($firstname);
+$firstname = strtolower($firstname);
+$filterfirstname = filter_var($firstname, FILTER_SANITIZE_STRING);
+$cleanfirstname = mysqli_real_escape_string($con, $filterfirstname);
+
+//last name
+$lastname = $_POST["lastname"];
+$lastname = trim($lastname);
+$lastname = strtolower($lastname);
+$filterlastname = filter_var($lastname, FILTER_SANITIZE_STRING);
+$cleanlastname = mysqli_real_escape_string($con, $filterlastname);
+
+//phone
+$phone = $_POST["phone"];
+$phone = trim($phone);
+$filterphone = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+$cleanphone= mysqli_real_escape_string($con, $filterphone);
+
+//mobile
+$mobile = $_POST["mobile"];
+$mobile = trim($mobile);
+$filtermobile = filter_var($mobile, FILTER_SANITIZE_NUMBER_INT);
+$cleanmobile= mysqli_real_escape_string($con, $filtermobile);
+
+//email
+$email = $_POST["email"];
+$email = trim($email);
+$email = strtolower($email);
+$filteremail = filter_var($email, FILTER_SANITIZE_EMAIL);
+$cleanemail = mysqli_real_escape_string($con, $filteremail);
+
+//fax
+$fax = $_POST["fax"];
+$fax = trim($fax);
+$filterfax = filter_var($fax, FILTER_SANITIZE_NUMBER_INT);
+$cleanfax= mysqli_real_escape_string($con, $filterfax);
+
+//job title
+$jobtitle = $_POST["jobtitle"];
+$jobtitle = trim($jobtitle);
+$jobtitle = strtolower($jobtitle);
+$filterjobtitle = filter_var($jobtitle, FILTER_SANITIZE_STRING);
+$cleanjobtitle= mysqli_real_escape_string($con, $filterjobtitle);
+
+/*
+//echo $creationdate;
+echo $cleanjobtype.'<br>';
+echo $cleanjobdescription.'<br>';
+echo $cleanstatus.'<br>';
+echo $cleandate.'<br>';
+//echo $cleantime.'<br>';
+echo $creationdate.'<br>';
+echo $cleansagereference.'<br>';
+echo $cleanponumber.'<br>';
+echo $cleanjobnumber.'<br>';*/
+
+$sql = "INSERT INTO workers (first_name, last_name, phone_num, mobile_phone_num, email, fax, job_title) VALUES ('$cleanfirstname', '$cleanlastname', '$cleanphone', '$cleanmobile', '$cleanemail', '$cleanfax', '$cleanjobtitle'); ";
+
+$res = mysqli_query($con,$sql);
+//echo $sql;
+
+$sql2 = "SELECT workerid FROM workers ORDER BY workerid DESC LIMIT 1; ";
+$result = mysqli_query($con,$sql2);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $workerid = $row["workerid"];
+		//echo $workerid;
+		//echo $companyid;
+    }
+} else {
+    echo "0 results";
+}
+
+$sql3 = "INSERT INTO works_with (workerid, companyid) VALUES ('$workerid', '$companyid'); ";
+
+$result2 = mysqli_query($con,$sql3);
+mysqli_close($con);
+echo'<!DOCTYPE html>
+<html>
+	<head>
+	<title>Contact Created</title>
+	<link href="css/elements.css" rel="stylesheet">
+	<script src="js/popup.js"></script>
+	</head>
+<!-- Body Starts Here -->
+	<body>
+	<div id="body" style="overflow:hidden;">
+		<div id="abc">
+			<!-- Popup Div Starts Here -->
+			<div id="popupContact">
+			<!-- Contact Us Form -->
+				<form action="" id="form" method="post" name="form">
+					<!--<img id="close" src="images/3.png" onclick ="div_hide()">-->
+					<h2>Job Created</h2>
+					<hr>
+					<a href="'.$url.'" id="submit">OK</a>
+				</form>
+			</div>
+		<!-- Popup Div Ends Here -->
+		</div>
+	</div>
+	</body>
+	
+	<script type="text/javascript">
+	function goBack() {
+		window.history.go(-2);
+	}
+	window.onload = div_show();
+	</script>
+<!-- Body Ends Here -->
+</html>';
+?>
