cfc21ac2a02661da8d58bf93db1619738f0d009b
Colin Curran <col.w.curran@gmail.com>
2016-05-30 11:59:38 +0100

    from for completed prospecting tasks

---
+++ prospecting_results.php
@@ -0,0 +1,95 @@
+<?php
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "user");
+define("DB_PASSWORD", "1234");
+define("DB_DATABASE", "database");
+ 
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+/*
+$url= $_GET['url'];
+$customerid= $_GET['customerid'];
+$companyid= $_GET['companyid'];
+*/
+$url = 2;
+$customerid = 3;
+$companyid = 7;
+echo'
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Result</title>
+	<link href="css/elements.css" rel="stylesheet">
+	<script src="js/popup.js"></script>
+	</head>
+<!-- Body Starts Here -->
+	<body>
+	<div id="body" style="overflow:hidden;">
+		<div id="abc">
+			<!-- Popup Div Starts Here -->
+			<div id="popupContact">
+			<!-- Contact Us Form -->
+				<form action="" id="form" method="post" name="form">
+					<!--<img id="close" src="images/3.png" onclick ="div_hide()">-->
+					<h2>Result</h2>
+					<hr>
+					<input type="hidden" name="url" id="url" value="'.$url.'">
+					<input type="hidden" name="customerid" id="customerid" value="'.$customerid.'">
+					<input type="hidden" name="companyid" id="companyid" value="'.$companyid.'">
+					
+					<label for="activitytype"><small>Type</small></label><br>
+					<select id="activitytype"class="drop_down"  name = "activitytype" class="form-control">
+						<option value= "phone call">Phone call</option>
+						<option value= "door step">Door step</option>
+						<option value= "left brochure">Left brochure</option>
+						<option value= "left business card">Left business card</option>
+						<option value= "courtesy call">Courtesy call</option>
+						<option value= "demo">Demo</option>
+						<option value= "c.p.d">C.P.D</option>
+						<option value= "followup meeting">Followup meeting</option>
+						<option value= "other">Other</option>
+					</select><br>
+										
+					<label for="result"><small>Result</small></label><br>
+					<select id="result" class="drop_down"  name = "result" class="form-control">
+						<option value= "no interest">No interest</option>
+						<option value= "not available">Not available</option>
+						<option value= "got an appointment">Got an appointment</option>
+						<option value= "other">Other</option>
+					</select>
+					
+					<label for="description"><small>Description</small></label>
+					<textarea maxlength="200" class ="form-textarea" id="description" name="description" type="text"></textarea>
+					
+					<label for="next_action"><small>Next action</small></label>
+					<select id="next_action" class="drop_down"  name = "next_action" class="form-control">
+						<option value= "no further action">No further action</option>
+						<option value= "call again">Call again</option>
+						<option value= "schedule qualify meeting">Schedule qualify meeting</option>
+						<option value= "schedule presentation meeting">Schedule presentation meeting</option>
+						<option value= "schedule quote meeting">Schedule quote meeting</option>
+						<option value= "schedule close meeting">Schedule close meeting</option>
+					</select><br>
+					
+					<label for="date"><small>Due date</small></label>
+					<input id="date" name="date" type="date" required>
+					
+					<label for="time"><small>Time</small></label>
+					<input id="time" name="time" type="time" required>
+					
+					<input type="submit" id="submit" value="Save">
+					<!--<a href="javascript:%20check_empty()" id="submit">Save</a>-->
+					<a onclick="goBack()" id="submit">Cancel</a>
+				</form>
+			</div>
+		<!-- Popup Div Ends Here -->
+		</div>
+	</div>
+	</body>
+	<script type="text/javascript">
+	window.onload = div_show();
+	</script>
+
+<!-- Body Ends Here -->
+</html>';
+mysqli_close($con);
+?>
