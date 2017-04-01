7c14ffe1ee4f88b488b2e064413481f2ead4c15d
Colin Curran <col.w.curran@gmail.com>
2017-04-01 16:53:00 +0100

    home time

--- send_sms.php
+++ send_sms.php
@@ -2,21 +2,25 @@
 
 //print_r($_POST['numbers']);
 $nums = array();
-foreach($_POST['numbers'] as $n) {
-			array_push($nums,
-			urlencode($n)
-			);
-		}
+if(isset($_POST['numbers'])){
+	foreach($_POST['numbers'] as $n) {
+		//this adds the international calling prefix to the number (ie 353)
+		$n = preg_replace('/^0/','353',$n);
+				array_push($nums,
+				urlencode($n)
+				);
+			}
 	print_r (array_values($nums));
+}
 
 	$username = urlencode("colin.c");
 	$password = urlencode("SKDcGDUSMfLHAB");
 	$api_id = urlencode("3619671");
 	//$number = $_POST['mobilenumber'];
-	$toList = array(urlencode("353851084442"),urlencode("number 3"));
+	//$toList = array(urlencode("353851084442"),urlencode("number 3"));
 	//$toList = array(urlencode($number),urlencode("number 2"),urlencode("number 3"));
 	 
-	$to = implode(',', $toList);
+	$to = implode(',', $nums);
 	$message = "message";
 	//$message = $_POST['message'];
 	$message = urlencode($message);
